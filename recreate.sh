39daf67ac9a0ebf7e065f361b32a6db5f04097f6
Mick Watson <mick.watson@roslin.ed.ac.uk>
2019-05-08 09:33:50 +0100

    Moving results to results

---
+++ recreate.sh
@@ -0,0 +1,11 @@
+cp Snakefile.pig5 Snakefile.pig100
+sed -i 's/pig5/pig100/' Snakefile.pig100
+cp Snakefile.pig5 Snakefile.pig15
+sed -i 's/pig5/pig15/' Snakefile.pig15
+cp Snakefile.pig5 Snakefile.pig238
+sed -i 's/pig5/pig238/' Snakefile.pig238
+cp Snakefile.pig5 Snakefile.pig25
+sed -i 's/pig5/pig25/' Snakefile.pig25
+cp Snakefile.pig5 Snakefile.pig50
+sed -i 's/pig5/pig50/' Snakefile.pig50
+
